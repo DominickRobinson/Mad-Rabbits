1fc6c83c43380badf20bdeade097ea3e80464e39
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2022-11-19 15:25:45 -0500

    7

--- Scenes/Characters/Rex.gd
+++ Scenes/Characters/Rex.gd
@@ -2,11 +2,56 @@ extends Rabbit
 const Explosion = preload("../Effects/Explosion.tscn")
 const Kaboom = preload("../Effects/Kaboom.tscn")
 
+
+export (float) var floatiness := 0.2
+
+
+
+var og_gravity_scale = gravity_scale
+var shoot_angle = null
+
 func _ready():
 	catchphrase_text = "d e s t r o y"
+	$Sword.visible = false
+	$Sword.disabled = true
+	ability_slowmo_scale = 0.9
+	zoom_in_duration = 1.55
 
 
+#glock
 func ability1():
+
+	$Gun.fire()
+	angular_velocity = 0
+	var temp_lin_vel = linear_velocity
+	linear_velocity *= 0
+	linear_damp = 10
+	
+
+	freeze()
+	if shoot_angle != null:
+		global_rotation_degrees = shoot_angle
+		print("shooting at: ", shoot_angle)
+	angular_damp = 10
+	
+	ability_used = true
+	gravity_scale = floatiness * og_gravity_scale
+	self_modulate = Color.red
+	
+	yield(get_tree().create_timer(zoom_in_duration), "timeout")
+	unfreeze()
+	stop_shooting()
+	#linear_velocity = temp_lin_vel
+
+
+#lightsaber
+func ability2():
+	$Sword.visible = true
+	$Sword.disabled = false
+
+
+#explosion
+func ability3():
 	ability_used = true
 	explode()
 	
@@ -34,3 +79,19 @@ func explode():
 	hideCatchphrase()
 	Manager.findCamera().abilityZoomOut()
 	queue_free()
+
+
+
+func stop_shooting():
+	#print("anim done")
+	stop_floating()
+	angular_damp = -1
+	linear_damp = -1
+
+func recoil():
+	var dir = Vector2(cos(global_rotation), sin(global_rotation))
+	#linear_velocity += -5*dir
+	apply_central_impulse(-2*dir)
+
+func stop_floating():
+	gravity_scale = og_gravity_scale
