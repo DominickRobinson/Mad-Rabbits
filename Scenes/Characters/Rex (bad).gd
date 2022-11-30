9af75bdbed78afd0afd100c7a325e2c9826f6755
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2022-11-30 12:28:37 -0500

    fixed Rex gun

--- Scenes/Characters/Rex (bad).gd
+++ Scenes/Characters/Rex (bad).gd
@@ -22,18 +22,24 @@ func _ready():
 	else:
 		get_node("Infinity Carrot").visible = false
 
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("ability") and currentAbility == 1 and state == RabbitState.thrown and not ability_used:
+		shoot_angle = rad2deg(get_global_mouse_position().angle_to_point(global_position))
+		#print(shoot_angle)
 
-#glock
+#shoot
 func ability1():
 	$Gun.fire()
 	angular_velocity = 0
 	var temp_lin_vel = linear_velocity
 	linear_velocity *= 0
 	linear_damp = 10
+	
 
 	freeze()
 	if shoot_angle != null:
 		global_rotation_degrees = shoot_angle
+		#print("shooting at: ", shoot_angle)
 	angular_damp = 10
 	
 	ability_used = true
@@ -44,6 +50,20 @@ func ability1():
 	unfreeze()
 	stop_shooting()
 	#linear_velocity = temp_lin_vel
+
+func stop_shooting():
+	#print("anim done")
+	stop_floating()
+	angular_damp = -1
+	linear_damp = -1
+
+func recoil():
+	var dir = Vector2(cos(global_rotation), sin(global_rotation))
+	#linear_velocity += -5*dir
+	apply_central_impulse(-2*dir)
+
+func stop_floating():
+	gravity_scale = og_gravity_scale
 
 
 
@@ -91,16 +111,43 @@ func explode():
 
 
 
-func stop_shooting():
-	#print("anim done")
-	stop_floating()
-	angular_damp = -1
-	linear_damp = -1
 
-func recoil():
-	var dir = Vector2(cos(global_rotation), sin(global_rotation))
-	#linear_velocity += -5*dir
-	apply_central_impulse(-2*dir)
 
-func stop_floating():
-	gravity_scale = og_gravity_scale
+
+
+##glock
+#func ability1():
+#	$Gun.fire()
+#	angular_velocity = 0
+#	var temp_lin_vel = linear_velocity
+#	linear_velocity *= 0
+#	linear_damp = 10
+#
+#	freeze()
+#	if shoot_angle != null:
+#		global_rotation_degrees = shoot_angle
+#	angular_damp = 10
+#
+#	ability_used = true
+#	gravity_scale = floatiness * og_gravity_scale
+#	self_modulate = Color.red
+#
+#	yield(get_tree().create_timer(zoom_in_duration), "timeout")
+#	unfreeze()
+#	stop_shooting()
+#	#linear_velocity = temp_lin_vel
+
+
+#func stop_shooting():
+#	#print("anim done")
+#	stop_floating()
+#	angular_damp = -1
+#	linear_damp = -1
+#
+#func recoil():
+#	var dir = Vector2(cos(global_rotation), sin(global_rotation))
+#	#linear_velocity += -5*dir
+#	apply_central_impulse(-2*dir)
+#
+#func stop_floating():
+#	gravity_scale = og_gravity_scale
