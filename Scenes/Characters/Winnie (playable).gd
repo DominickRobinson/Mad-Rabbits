b4553a579b7819def4f62b57a8a55935e3bb393f
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2022-11-30 23:04:27 -0500

    bnirr

---
+++ Scenes/Characters/Winnie (playable).gd
@@ -0,0 +1,16 @@
+extends Rabbit
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func ability1():
+	modulate = Color.red
+	ability_used = true
